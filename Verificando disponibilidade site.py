9216c26b12c8f045cc14d527fe86ec7e039b6558
Gustavo Felix | Stone <gustavo.felix@stone.com.br>
2022-02-07 16:22:38 -0300

    alterando Verificando disponibilidade site e add index1.html

--- Verificando disponibilidade site.py
+++ Verificando disponibilidade site.py
@@ -1,11 +1,13 @@
 import urllib 
 import urllib.request
-
-
 try:
-  site = urllib.request.urlopen('http://www.facebook.com.br')
+ site = urllib.request.urlopen('file:///C:/Users/gustavo.felix/Documents/git-e-github/Gustavo/Python-Basico/index1.html')
 except urllib.error.URLError:
   print('Não consegui conectar')
 else:
   print('Consegui conectar')
   
+pagina = urllib.request.urlopen('file:///C:/Users/gustavo.felix/Documents/git-e-github/Gustavo/Python-Basico/index1.html')
+texto = pagina.read().decode("utf8")
+
+print(texto)
